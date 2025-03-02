0af9c9e954829ac9be2ad4e94eaa9d9489ddb1e7
Miracle Kasigwa <miraclekasigwa@gmail.com>
2025-03-02 21:36:45 +0100

    deployed authentication feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'sso_db',
+      entities: [User],
+      synchronize: true, // Auto create tables (disable in production)
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
